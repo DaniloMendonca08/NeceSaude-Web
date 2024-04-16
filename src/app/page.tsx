6a6005a1b53d97b37479d1dd08a27983993c399b
Danilo Mendonça <danilomendonca08@gmail.com>
2024-04-16 17:04:18 -0300

    Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen">
       <NavBar />
-      <div className="w-64">
-        <h2 className="text-slate-700">Login</h2>
+      <div className="w-1/5 bg-custom-blue grid grid-cols-1 gap-8 justify-center items-center my-20 mx-auto p-10 rounded-lg text-center">
+        <h2>Login</h2>
         <Input 
-        key="nome"
-        label = "Nome"
-        name = "nome"
-        labelPlacement="outside"/>
+        key="email"
+        label = "Email"
+        name = "email"
+        labelPlacement="outside"
+        />
+
+        <Input 
+        key="senha"
+        label = "Senha"
+        name = "senha"
+        labelPlacement="outside"
+        />
       </div>
     </main>
   );
