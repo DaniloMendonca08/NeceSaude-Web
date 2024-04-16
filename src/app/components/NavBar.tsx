9f1345b48099b65622168fca000eec8e9bdf0aee
Danilo Mendonça <danilomendonca08@gmail.com>
2024-04-15 22:54:47 -0300

    iniciacao do projeto

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,8 @@
+export default function NavBar() {
+    return (
+        <nav className="bg-custom-blue w-full px-6 py-4">
+            <h1 className="text-center">NeceSaúde</h1>
+        </nav>
+    )
+
+}
